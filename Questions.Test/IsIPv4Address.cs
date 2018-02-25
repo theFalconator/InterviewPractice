849885b7bb15bcc1e16fdbd106fe86d7222d1efb
theFalconator <falcon.nick@gmail.com>
2018-02-25 17:04:47 -0600

    solution accepted

---
+++ Questions.Test/IsIPv4Address.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class IsIPv4Address
+    {
+        [Fact]
+        public void AllNumbersMustBeBetween0And255()
+        {
+            var ip = new IPv4();
+            const string input = "172.16.254.1";
+            const string fails = "256.1.1.2";
+            Assert.True(ip.isIPv4Address(input));
+            Assert.False(ip.isIPv4Address(fails));
+        }
+
+        [Fact]
+        public void ShouldOnlyBeThreeDots()
+        {
+            var ip = new IPv4();
+            const string input = "172.16.254.1";
+            const string fails = "172.16.254.1.2";
+            Assert.True(ip.isIPv4Address(input));
+            Assert.False(ip.isIPv4Address(fails));
+        }
+
+        [Fact]
+        public void ShouldHaveFourNumbers()
+        {
+            var ip = new IPv4();
+            const string input = ".16.254.1";
+            Assert.False(ip.isIPv4Address(input));
+        }
+
+        [Fact]
+        public void BoundsCheck()
+        {
+            var ip = new IPv4();
+            const string input = "0.254.255.0";
+            Assert.True(ip.isIPv4Address(input));
+        }
+
+    }
+}
---
+++ Questions/IPv4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questions
+{
+    public class IPv4
+    {
+        public bool isIPv4Address(string inputString)
+        {
+            const int maxIPv4Decimal = 255;
+            const int minIPv4Decimal = 0;
+            const int numDots = 3;
+            const int nums = 4;
+            try
+            {
+                var decimals = inputString.Split('.').Select(int.Parse);
+                var dots = inputString.Count(c => c == '.');
+
+                return decimals.All(i => i <= maxIPv4Decimal)
+                       && decimals.All(i => i >= minIPv4Decimal)
+                       && dots == numDots
+                       && decimals.Count() == nums;
+            }
+            catch (Exception e)
+            {
+                // ".254.255.0" will throw an exception
+                // not a valid address so return false
+                return false;
+            }
+        }
+    }
+}
