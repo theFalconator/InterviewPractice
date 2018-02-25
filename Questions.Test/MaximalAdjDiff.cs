695c8673d4c3fe96d5f0bb3499cd1064f584204d
theFalconator <falcon.nick@gmail.com>
2018-02-25 16:37:33 -0600

    solution accepted

---
+++ Questions.Test/MaximalAdjDiff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class MaximalAdjDiff
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            var input = new[] {2, 4, 1, 0};
+            var diff = new MaximalAdjacentDifference();
+            Assert.Equal(3, diff.arrayMaximalAdjacentDifferrence(input));
+        }
+    }
+}
---
+++ Questions/MaximalAdjacentDifference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Questions
+{
+    public class MaximalAdjacentDifference
+    {
+        public int arrayMaximalAdjacentDifferrence(int[] inputArray)
+        {
+            // offset input array by 1
+            var offsetInputArray = inputArray.Skip(1);
+            
+            // first - second
+            var diffs = inputArray.Zip(offsetInputArray, (x, y) => x - y);
+
+            // second - first
+            var diffs2 = inputArray.Zip(offsetInputArray, (x, y) => y - x);
+
+            // maximum from both cases
+            var result = new[] {diffs.Max(), diffs2.Max()};
+
+            // return largest
+            return result.Max();
+        }
+    }
+}
