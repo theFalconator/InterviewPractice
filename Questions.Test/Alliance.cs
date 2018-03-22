f2d68f6dce31447c656ab4c43356a674b84d1b12
theFalconator <falcon.nick@gmail.com>
2018-03-21 19:16:04 -0500

    alliance stuff

---
+++ Questions.Test/Alliance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class Alliance
+    {
+        [Fact]
+        public void InstantBuild()
+        {
+            var alliance = new AllianceHelp();
+            Assert.Equal(0, alliance.allianceHelp(1000,10));
+        }
+
+        [Fact]
+        public void OneMemberAlliance()
+        {
+            var alliance = new AllianceHelp();
+            Assert.Equal(40, alliance.allianceHelp(100, 1));
+        }
+
+        [Fact]
+        public void ROunding()
+        {
+            var alliance = new AllianceHelp();
+            Assert.Equal(9, alliance.allianceHelp(999, 11));
+        }
+
+        [Fact]
+        public void WinAgainstMonster()
+        {
+            var alliance = new AllianceHelp();
+            Assert.Equal(2, alliance.allianceVersusMonster(new [] {110,30,50}, new [] {12,11,20}));
+        }
+
+        [Fact]
+        public void FailAgainstMonster()
+        {
+            var alliance = new AllianceHelp();
+            Assert.Equal(0, alliance.allianceVersusMonster(new[] { 4,10,10,10 }, new[] { 10,1,1,1 }));
+        }
+    }
+}
---
+++ Questions/AllianceHelp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questions
+{
+    public class Warrior
+    {
+        public int HP { get; set; }
+        public int Attack { get; set; }
+    }
+
+    public class AllianceHelp
+    {
+        public int allianceHelp(int t, int allianceSize)
+        {
+            int boosts = 0;
+            int tenth = (int) Math.Floor(t * 0.1);
+
+            while (allianceSize > 0 && boosts < 10)
+            {
+                if (tenth > 60)
+                {
+                    t -= tenth;
+                    allianceSize--;
+                    boosts++;
+                }
+                else
+                {
+                    t -= 60;
+                    allianceSize--;
+                    boosts++;
+                }
+            }
+
+            if (t < 0)
+                return 0;
+            return t;
+        }
+
+        public int allianceVersusMonster(int[] healthPoints, int[] attackDamage)
+        {
+            var monsterHP = healthPoints.First();
+            var monsterAttack = attackDamage.First();
+
+            var warriors = new List<Warrior>();
+            for (var i = 1; i < healthPoints.Length; i++)
+            {
+                warriors.Add(new Warrior() {Attack = attackDamage[i], HP = healthPoints[i]});
+            }
+
+
+            while (monsterHP > 0)
+            {
+                // start with strongest alive warrior and hit until he is dead then move to next
+                try
+                {
+                    var w = warriors.Where(warrior => warrior.HP > 0).OrderBy(warrior => warrior.Attack).First();
+
+
+                    monsterHP -= w.Attack;
+                    w.HP -= monsterAttack;
+                    if (w.HP < 0)
+                    {
+                        warriors.Remove(w);
+                    }
+
+                    
+                }
+                catch (Exception e)
+                {
+                    return 0;
+                }
+
+            }
+            return warriors.Count()+1;
+
+        }
+    }
+}
