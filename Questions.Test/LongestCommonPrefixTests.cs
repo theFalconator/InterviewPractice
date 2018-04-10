09fdea2df980ce25f5ca0e98bef2a2019f870660
theFalconator <falcon.nick@gmail.com>
2018-04-10 18:44:09 -0500

    solved longest common prefix

---
+++ Questions.Test/LongestCommonPrefixTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class LongestCommonPrefixTests
+    {
+        [Fact]
+        public void StringsCanShareCommonPrefix()
+        {
+            var input = new[] {"aab", "aabcd"};
+            var lcp = new LongestCommonPrefix();
+            Assert.Equal("aab", lcp.longestCommonPrefix(input));
+        }
+
+        [Fact]
+        public void NoCommonPrefixReturnsEmptyString()
+        {
+            var input = new[] {"cat", "bat", "hat"};
+            var lcp = new LongestCommonPrefix();
+            Assert.Equal("", lcp.longestCommonPrefix(input));
+        }
+
+        [Fact]
+        public void ReturnsEmptyStringForNoInput()
+        {
+            var lcp = new LongestCommonPrefix();
+            Assert.Equal("", lcp.longestCommonPrefix(null));
+        }
+    }
+}
---
+++ Questions/LongestCommonPrefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questions
+{
+    public class LongestCommonPrefix
+    {
+        public string longestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+                return "";
+
+            var sortedStrings = strs.OrderBy(x => x.Length).ToArray();
+            string prefix = "";
+            for (int i = 0; i < sortedStrings.First().Length; i++)
+            {
+                if (i > 0 && string.IsNullOrEmpty(prefix))
+                    return "";
+
+                if (sortedStrings.All(s => s[i] == sortedStrings.First()[i]))
+                    prefix += sortedStrings.First()[i];
+
+            }
+
+            return prefix;
+        }
+    }
+}
