7fddef4eac4bc34feca499daade24241d6cced5a
theFalconator <falcon.nick@gmail.com>
2018-04-10 19:14:11 -0500

    accepted valid parens solution

---
+++ Questions.Test/ValidParenthesesTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class ValidParenthesesTests
+    {
+        [Theory]
+        [InlineData("()")]
+        [InlineData("[]")]
+        [InlineData("{}")]
+        public void SingleBracketTypeClosesCorrectly(string parens)
+        {
+            var vp = new ValidParentheses();
+
+            Assert.True(vp.IsValid(parens));
+        }
+
+        [Theory]
+        [InlineData("()[]{}")]
+        [InlineData("([{}])")]
+        public void BracketsCloseInCorrectOrder(string parens)
+        {
+            var vp = new ValidParentheses();
+
+            Assert.True(vp.IsValid(parens));
+        }
+
+        [Theory]
+        [InlineData("(]")]
+        [InlineData("([)]")]
+        public void BracketsClosingInWrongOrderShouldReturnFalse(string parens)
+        {
+            var vp = new ValidParentheses();
+
+            Assert.False(vp.IsValid(parens));
+        }
+
+        [Fact]
+        public void DoesntTryToPopAnEmptyStack()
+        {
+            var vp = new ValidParentheses();
+
+            // Assert.False(vp.IsValid("]"));
+            Assert.False(vp.IsValid("){"));
+        }
+    }
+}
---
+++ Questions/ValidParentheses.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questions
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return false;
+
+            var opening = new[] {"(", "[", "{"};
+
+            if (s.Length == 1 && !opening.Any(s.Contains))
+                return false;
+
+            Stack<char> closingStack = new Stack<char>();
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        closingStack.Push(')');
+                        break;
+                    case '[':
+                        closingStack.Push(']');
+                        break;
+                    case '{':
+                        closingStack.Push('}');
+                        break;
+                    default:
+                        if (closingStack.Count == 0)
+                            return false;
+                        if (c != closingStack.Peek())
+                            return false;
+
+                        closingStack.Pop();
+                        break;
+                }
+            }
+            return closingStack.Count == 0;
+        }
+    }
+}
