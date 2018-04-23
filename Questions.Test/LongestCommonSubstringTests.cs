4ac60385341ae4910b73978bbb1e19e965536d37
theFalconator <falcon.nick@gmail.com>
2018-04-23 10:24:16 -0500

    two pointer solution with Dictionary

---
+++ Questions.Test/LongestCommonSubstringTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class LongestCommonSubstringTests
+    {
+        [Fact]
+        public void EmptyStringsReturnZero()
+        {
+            var lcs = new LongestCommonSubstring();
+
+            Assert.Equal(0, lcs.LengthOfLongestSubstring(""));
+        }
+
+        [Fact]
+        public void CanFindSubstringAtBeginning()
+        {
+            var input = "abcabcbb";
+            var expected = "abc";
+
+            var lcs = new LongestCommonSubstring();
+
+            Assert.Equal(expected.Length, lcs.LengthOfLongestSubstring(input));
+        }
+
+        [Fact]
+        public void CanFindSubstringInMiddle()
+        {
+            var input = "pwwkew";
+            var expected = "wke";
+
+            var lcs = new LongestCommonSubstring();
+
+            Assert.Equal(expected.Length, lcs.LengthOfLongestSubstring(input));
+        }
+    }
+}
---
+++ Questions/LongestCommonSubstring.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questions
+{
+    public class LongestCommonSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            var map = new Dictionary<char, int>();
+            var max = 0;
+            var j = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                
+                if (map.ContainsKey(s[i]))
+                {
+                    j = Math.Max(j, map[s[i]] + 1);
+                    map[s[i]] = i;
+                }
+                else
+                {
+                    map.Add(s[i], i);
+                }
+                
+                
+                max = Math.Max(max, i - j + 1);
+            }
+
+            return max;
+
+        }
+    }
+}
