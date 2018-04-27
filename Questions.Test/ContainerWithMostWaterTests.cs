abaf2824109ea8413bfe2af1371b43f6c894dfdd
theFalconator <falcon.nick@gmail.com>
2018-04-27 18:15:04 -0500

    time limit exceeded

---
+++ Questions.Test/ContainerWithMostWaterTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Questions.Test
+{
+    public class ContainerWithMostWaterTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public ContainerWithMostWaterTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void NotEnoughLinesReturnsNegativeOne()
+        {
+            var container = new ContainerWithMostWater();
+            var lines = new[] {1};
+            Assert.Equal(-1, container.MaxArea(lines));
+        }
+
+        [Fact]
+        public void OnlyTwoLinesReturnsArea()
+        {
+            var container = new ContainerWithMostWater();
+            var lines = new[] {1, 3};
+            Assert.Equal(1, container.MaxArea(lines));
+        }
+
+        [Fact]
+        public void SampleInputShouldReturn49()
+        {
+            var sw = new Stopwatch();
+            
+            var container = new ContainerWithMostWater();
+
+            var lines = new[] {1, 8, 6, 2, 5, 4, 8, 3, 7};
+            
+            Assert.Equal(49, container.MaxArea(lines));
+        }
+    }
+}
