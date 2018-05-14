fe30a42f30f82052fada0b9a30a5ea3087c1376e
theFalconator <falcon.nick@gmail.com>
2018-05-14 07:12:34 -0500

    partial linq solution for numerals, need to add more tests

---
+++ Questions.Test/IntegerToRomanTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Questions.Test
+{
+    public class IntegerToRomanTests
+    {
+        [Fact]
+        public void BasicTen()
+        {
+            var itor = new IntegerToRoman();
+            Assert.Equal("I", itor.intToRoman(1));
+            Assert.Equal("II", itor.intToRoman(2));
+            Assert.Equal("III", itor.intToRoman(3));
+            Assert.Equal("IV", itor.intToRoman(4));
+            Assert.Equal("V", itor.intToRoman(5));
+            Assert.Equal("VI", itor.intToRoman(6));
+            Assert.Equal("VII", itor.intToRoman(7));
+            Assert.Equal("VIII", itor.intToRoman(8));
+            Assert.Equal("IX", itor.intToRoman(9));
+            Assert.Equal("X", itor.intToRoman(10));
+        }
+
+        [Fact]
+        public void Teens()
+        {
+            var itor = new IntegerToRoman();
+            Assert.Equal("XIV", itor.intToRoman(14));
+            Assert.Equal("XIX", itor.intToRoman(19));
+        }
+    }
+}
---
+++ Questions/IntegerToRoman.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questions
+{
+    public class IntegerToRoman
+    {
+        public string intToRoman(int num)
+        {
+
+            var numerals = new Dictionary<string, int>
+            {
+                {"I", 1},
+                {"IV", 4},
+                {"V", 5},
+                {"IX", 9},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000}
+            };
+
+            var result = new StringBuilder();
+
+            while (num > 0)
+            {
+                var kvp = numerals
+                    .Where(n => n.Value <= num)
+                    .OrderBy(n => n.Value)
+                    .Last();
+                num -= kvp.Value;
+                result.Append(kvp.Key);
+            }
+
+            return result.ToString();
+        }
+    }
+}
